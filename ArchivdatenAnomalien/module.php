c77444e8bb519ed702d96fcdf4474aa5d11b9808
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-05-15 15:04:36 +0200

    Log

--- ArchivdatenAnomalien/module.php
+++ ArchivdatenAnomalien/module.php
@@ -42,12 +42,12 @@ declare(strict_types=1);
 				$values = AC_GetAggregatedValues($archiveID, $variableID, $aggregationType, $startDate, $endDate, 0);
 				$filteredValues = $this->filter_variable($values, $rawData);
 
-				IPS_LogMessage('aggregierte Values', print_r($filteredValues,true));
-
 				foreach ($filteredValues as $Value) {
 					$startDate = strtotime($Value['Date']) - 8640; //Value Datum - ein Tag
 					$endDate = strtotime($Value['Date']) + 8640; //Value Datum + ein Tag
 			
+					IPS_LogMessage('endDatum', date('d.m.Y H.i',$endDate));
+
 					$rawValues = AC_GetLoggedValues($archiveID, $variableID, $startDate, $endDate, 0);
 					$filteredRawValues = $this->filter_variable($rawValues,true);
 					if (count($filteredRawValues) >0 ) {
